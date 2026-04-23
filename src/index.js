1f68861004c5aaf3766f79526807c296fe912dba
d-yaahya <143932713+d-yaahya@users.noreply.github.com>
2026-04-24 00:04:44 +0300

    all wane

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./css/style.css";
+import "./sass/custom.scss";
+
+console.log("Hi yahya learning las go")
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack"; // to access built-in plugins
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import loader from "mini-css-extract-plugin";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    
+    port: 9000,
+    hot: false,
+    devMiddleware: {
+      writeToDisk: true,
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          }
+          ,
+           "css-loader",
+           "sass-loader"
+        ],
+      },
+      
+    ],
+  },
+  
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+       template: "./src/index.html"
+       }),
+       new HtmlWebpackPlugin({
+      filename: "stuodents1.html",
+       template: "./src/stuodents1.html"
+       }),
+       new HtmlWebpackPlugin({
+      filename: "stuodents2.html",
+       template: "./src/stuodents2.html"
+       }),
+       new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+    new CssMinimizerPlugin()
+    
+  ],
+};
