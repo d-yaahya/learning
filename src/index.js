b4f303bfeb5f5aac7e12fb10fb09d8721773e1cc
d-yaahya <143932713+d-yaahya@users.noreply.github.com>
2026-04-27 00:38:33 +0300

    update navbar and sections

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/style.css";
 import "./sass/custom.scss";
+import '@fortawesome/fontawesome-free/js/all.min.js'
 
-console.log("Hi yahya learning las go")
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".main-navbar");
+  const slider = document.querySelector(".hero-carousel");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > slider.offsetHeight) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ export default {
         use: "babel-loader",
       },
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.(sa|sc|c)ss$/i,
         use: [
           {
@@ -47,6 +51,20 @@ export default {
            "css-loader",
            "sass-loader"
         ],
+      },
+            {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "./img/[name][ext]"
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "./font/[name][ext]"
+        }
       },
       
     ],
